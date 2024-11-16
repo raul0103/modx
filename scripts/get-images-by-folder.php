abe97dd5c0d446ed57a77f5558ab587e5602a9e6
raul0103 <rshakurov95@mail.ru>
2024-11-16 11:13:44 +0500

    upd

--- scripts/get-images-by-folder.php
+++ scripts/get-images-by-folder.php
@@ -2,33 +2,42 @@
 
 if (empty($directory)) return;
 
-$directory = MODX_BASE_PATH . $directory;
+$cache_name = md5(serialize($scriptProperties));
+$cache_options = [
+    xPDO::OPT_CACHE_KEY => 'default/get-images-by-folder/',
+];
 
-// Проверяем, существует ли папка
-if (!is_dir($directory)) {
-    return "[$directory] - directory does not exist";
-}
+if (!$images = $modx->cacheManager->get($cache_name, $cache_options)) {
+    $directory = MODX_BASE_PATH . $directory;
 
-// Получаем список всех файлов в папке
-$files = scandir($directory);
-
-// Создаем массив для путей к изображениям
-$images = [];
-
-// Перебираем файлы
-foreach ($files as $file) {
-    // Игнорируем "." и ".."
-    if ($file === '.' || $file === '..') {
-        continue;
+    // Проверяем, существует ли папка
+    if (!is_dir($directory)) {
+        return "[$directory] - directory does not exist";
     }
 
-    // Полный путь к файлу
-    $filePath = MODX_BASE_PATH . $directory . DIRECTORY_SEPARATOR . $file;
+    // Получаем список всех файлов в папке
+    $files = scandir($directory);
 
-    // Проверяем, является ли файл изображением
-    if (preg_match('/\.(jpg|jpeg|png|gif|webp)$/i', $file)) {
-        $images[] = str_replace(MODX_BASE_PATH, '', $filePath);
+    // Создаем массив для путей к изображениям
+    $images = [];
+
+    // Перебираем файлы
+    foreach ($files as $file) {
+        // Игнорируем "." и ".."
+        if ($file === '.' || $file === '..') {
+            continue;
+        }
+
+        // Полный путь к файлу
+        $filePath = MODX_BASE_PATH . $directory . DIRECTORY_SEPARATOR . $file;
+
+        // Проверяем, является ли файл изображением
+        if (preg_match('/\.(jpg|jpeg|png|gif|webp)$/i', $file)) {
+            $images[] = str_replace(MODX_BASE_PATH, '', $filePath);
+        }
     }
+
+    $modx->cacheManager->set($cache_name, $images, 0, $cache_options);
 }
 
 return $images;
