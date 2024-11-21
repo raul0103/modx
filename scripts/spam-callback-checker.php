4b316ecdebce1e820c6a780f1364d26adad0ad28
raul0103 <rshakurov95@mail.ru>
2024-11-21 14:28:59 +0500

    upd

--- scripts/spam-callback-checker.php
+++ scripts/spam-callback-checker.php
@@ -2,43 +2,83 @@
 
 /**
  * Скрипт для блокировки спама
- * Можно поставить на срабатываение обратной формы
+ * Сохраняет данные в callbacks.json
  */
 
-$cookie = [
-    'key' => 'callbacks',
-    'default' =>  [
-        'time' => time(),
-        'count' => 1
-    ] // Дефолные данные при создании куки
-];
-
-// Лимиты по времени указанные в секундах
-$limits = [
-    'time' => 60 * 60 * 4, // Секунды. Лимит действия блокировки (60*60*4 = 4 часа)
-    'count' => 2 // Кол-во разрешенных отправок формы
-];
-
-// Если нет текущих данных  - создать
-$is_spam = false;
-if (empty($_COOKIE[$cookie['key']])) {
-    setcookie($cookie['key'], json_encode($cookie['default']));
-} else {
-    $data = json_decode($_COOKIE[$cookie['key']], true);
-    $data['count'] = $data['count'] + 1; // Увеличили кол-во отправок формы
-
-    $difference = time() - $data['time'];
-
-    // Если прошел промежуток блокировки - начать проверку заново
-    if ($difference > $limits['time']) {
-        setcookie($cookie['key'], json_encode($cookie['default']));
+function getUserIP()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        // IP из заголовка HTTP_CLIENT_IP
+        return $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // IP из заголовка HTTP_X_FORWARDED_FOR (используется прокси)
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
     } else {
-        if ($data['count'] > $limits['count']) {
-            $is_spam = true;
-        }
-
-        setcookie($cookie['key'], json_encode($data));
+        // IP из REMOTE_ADDR
+        return $_SERVER['REMOTE_ADDR'];
     }
 }
 
-if ($is_spam) return false;
+// Конфигурация
+$file_path = MODX_BASE_PATH . '/callbacks.json'; // Путь к JSON-файлу
+$limits = [
+    'time' => 60 * 60 * 4, // 4 часа
+    'count' => 2           // Лимит отправок формы
+];
+
+// Текущий IP пользователя
+$user_ip = getUserIP();
+
+// Загрузка данных из файла
+if (!file_exists($file_path)) {
+    $data = [
+        'created_at' => date('Y-m-d'), // Дата создания файла
+        'ips' => []                   // Список IP с данными
+    ];
+    file_put_contents($file_path, json_encode($data, JSON_PRETTY_PRINT));
+} else {
+    $data = json_decode(file_get_contents($file_path), true);
+}
+
+// Проверка даты создания файла, удаление, если файл устарел (один день)
+if (date('Y-m-d') !== $data['created_at']) {
+    $data = [
+        'created_at' => date('Y-m-d'),
+        'ips' => []
+    ];
+    file_put_contents($file_path, json_encode($data, JSON_PRETTY_PRINT));
+}
+
+// Проверка IP пользователя
+$is_spam = false;
+if (isset($data['ips'][$user_ip])) {
+    $ip_data = $data['ips'][$user_ip];
+    $time_diff = time() - $ip_data['time'];
+
+    // Если прошло больше 4 часов, удалить запись
+    if ($time_diff > $limits['time']) {
+        unset($data['ips'][$user_ip]);
+    } else {
+        // Увеличить счетчик отправок и проверить лимит
+        $ip_data['count']++;
+        $data['ips'][$user_ip] = $ip_data;
+
+        if ($ip_data['count'] > $limits['count']) {
+            $is_spam = true;
+        }
+    }
+} else {
+    // Добавить нового пользователя
+    $data['ips'][$user_ip] = [
+        'time' => time(),
+        'count' => 1
+    ];
+}
+
+// Сохранение обновленных данных в файл
+file_put_contents($file_path, json_encode($data, JSON_PRETTY_PRINT));
+
+// Вернуть результат проверки
+if ($is_spam) {
+    return false; // Это спам
+}
