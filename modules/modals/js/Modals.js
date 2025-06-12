36793adf381648c3f0f6e73621e41232dfccbdc3
raul0103 <rshakurov95@mail.ru>
2025-06-12 23:17:58 +0400

    fix modals

--- modules/modals/js/Modals.js
+++ modules/modals/js/Modals.js
@@ -41,6 +41,8 @@ export default class Modals {
       }
     },
     close: (modal, close_callback) => {
+      this.current_opened = null;
+
       let modal_close_btns = modal.querySelectorAll(this.selectors.close);
       modal_close_btns.forEach((modal_close_btn) => {
         modal_close_btn.addEventListener("click", () => {
