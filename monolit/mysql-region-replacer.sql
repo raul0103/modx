0b23237e788dd561fa9339b23623323920723f59
raul0103 <rshakurov95@mail.ru>
2025-12-08 23:38:25 +0500

    upd city replacer

--- monolit/mysql-region-replacer.sql
+++ monolit/mysql-region-replacer.sql
@@ -1,42 +1,94 @@
 UPDATE modx_site_content SET
-    content = REPLACE(content, 'в Санкт-Петербурге', 'в Москве');
+    pagetitle   = REPLACE(pagetitle,   'в Санкт-Петербурге', 'в Москве'),
+    longtitle   = REPLACE(longtitle,   'в Санкт-Петербурге', 'в Москве'),
+    description = REPLACE(description, 'в Санкт-Петербурге', 'в Москве'),
+    introtext   = REPLACE(introtext,   'в Санкт-Петербурге', 'в Москве'),
+    content     = REPLACE(content,     'в Санкт-Петербурге', 'в Москве'),
+    menutitle   = REPLACE(menutitle,   'в Санкт-Петербурге', 'в Москве');
+    
 UPDATE modx_site_content SET
-    content = REPLACE(content, 'по Санкт-Петербургу', 'по Москве');
-UPDATE modx_site_content SET
-    content = REPLACE(content, 'Санкт-Петербурга', 'Москвы');
-UPDATE modx_site_content SET
-    content = REPLACE(content, 'Санкт-Петербург', 'Москва');
+    pagetitle   = REPLACE(pagetitle,   'по Санкт-Петербургу', 'по Москве'),
+    longtitle   = REPLACE(longtitle,   'по Санкт-Петербургу', 'по Москве'),
+    description = REPLACE(description, 'по Санкт-Петербургу', 'по Москве'),
+    introtext   = REPLACE(introtext,   'по Санкт-Петербургу', 'по Москве'),
+    content     = REPLACE(content,     'по Санкт-Петербургу', 'по Москве'),
+    menutitle   = REPLACE(menutitle,   'по Санкт-Петербургу', 'по Москве');
 
 UPDATE modx_site_content SET
-    content = REPLACE(content, 'Ленинградскую', 'Московскую');
-UPDATE modx_site_content SET
-    content = REPLACE(content, 'Ленинградская', 'Московская');
-UPDATE modx_site_content SET
-    content = REPLACE(content, 'по Ленинградской', 'по Московской');
-UPDATE modx_site_content SET
-    content = REPLACE(content, 'по Ленобласти', 'по Московской области');
-UPDATE modx_site_content SET
-    content = REPLACE(content, 'Ленобласть', 'Московская область');
+    pagetitle   = REPLACE(pagetitle,   'Санкт-Петербурга', 'Москвы'),
+    longtitle   = REPLACE(longtitle,   'Санкт-Петербурга', 'Москвы'),
+    description = REPLACE(description, 'Санкт-Петербурга', 'Москвы'),
+    introtext   = REPLACE(introtext,   'Санкт-Петербурга', 'Москвы'),
+    content     = REPLACE(content,     'Санкт-Петербурга', 'Москвы'),
+    menutitle   = REPLACE(menutitle,   'Санкт-Петербурга', 'Москвы');
 
 UPDATE modx_site_content SET
-    pagetitle   = REPLACE(pagetitle,   'ЛО', 'МО'),
-    longtitle   = REPLACE(longtitle,   'ЛО', 'МО'),
-    description = REPLACE(description, 'ЛО', 'МО'),
-    introtext   = REPLACE(introtext,   'ЛО', 'МО'),
-    content     = REPLACE(content,     'ЛО', 'МО');
+    pagetitle   = REPLACE(pagetitle,   'Санкт-Петербург', 'Москва'),
+    longtitle   = REPLACE(longtitle,   'Санкт-Петербург', 'Москва'),
+    description = REPLACE(description, 'Санкт-Петербург', 'Москва'),
+    introtext   = REPLACE(introtext,   'Санкт-Петербург', 'Москва'),
+    content     = REPLACE(content,     'Санкт-Петербург', 'Москва'),
+    menutitle   = REPLACE(menutitle,   'Санкт-Петербург', 'Москва');
 
 UPDATE modx_site_content SET
-    pagetitle   = REPLACE(pagetitle,   'СПб', 'МСК'),
-    longtitle   = REPLACE(longtitle,   'СПб', 'МСК'),
-    description = REPLACE(description, 'СПб', 'МСК'),
-    introtext   = REPLACE(introtext,   'СПб', 'МСК'),
-    content     = REPLACE(content,     'СПб', 'МСК'),
-    menutitle   = REPLACE(menutitle,   'СПб', 'МСК');
+    pagetitle   = REPLACE(pagetitle,   'Ленинградскую', 'Московскую'),
+    longtitle   = REPLACE(longtitle,   'Ленинградскую', 'Московскую'),
+    description = REPLACE(description, 'Ленинградскую', 'Московскую'),
+    introtext   = REPLACE(introtext,   'Ленинградскую', 'Московскую'),
+    content     = REPLACE(content,     'Ленинградскую', 'Московскую'),
+    menutitle   = REPLACE(menutitle,   'Ленинградскую', 'Московскую');
 
 UPDATE modx_site_content SET
-    pagetitle   = REPLACE(pagetitle,   'СПБ', 'МСК'),
-    longtitle   = REPLACE(longtitle,   'СПБ', 'МСК'),
-    description = REPLACE(description, 'СПБ', 'МСК'),
-    introtext   = REPLACE(introtext,   'СПБ', 'МСК'),
-    content     = REPLACE(content,     'СПБ', 'МСК'),
-    menutitle   = REPLACE(menutitle,   'СПБ', 'МСК');
+    pagetitle   = REPLACE(pagetitle,   'Ленинградская', 'Московская'),
+    longtitle   = REPLACE(longtitle,   'Ленинградская', 'Московская'),
+    description = REPLACE(description, 'Ленинградская', 'Московская'),
+    introtext   = REPLACE(introtext,   'Ленинградская', 'Московская'),
+    content     = REPLACE(content,     'Ленинградская', 'Московская'),
+    menutitle   = REPLACE(menutitle,   'Ленинградская', 'Московская');
+
+UPDATE modx_site_content SET
+    pagetitle   = REPLACE(pagetitle,   'по Ленинградской', 'по Московской'),
+    longtitle   = REPLACE(longtitle,   'по Ленинградской', 'по Московской'),
+    description = REPLACE(description, 'по Ленинградской', 'по Московской'),
+    introtext   = REPLACE(introtext,   'по Ленинградской', 'по Московской'),
+    content     = REPLACE(content,     'по Ленинградской', 'по Московской'),
+    menutitle   = REPLACE(menutitle,   'по Ленинградской', 'по Московской');
+
+UPDATE modx_site_content SET
+    pagetitle   = REPLACE(pagetitle,   'по Ленобласти', 'по Московской области'),
+    longtitle   = REPLACE(longtitle,   'по Ленобласти', 'по Московской области'),
+    description = REPLACE(description, 'по Ленобласти', 'по Московской области'),
+    introtext   = REPLACE(introtext,   'по Ленобласти', 'по Московской области'),
+    content     = REPLACE(content,     'по Ленобласти', 'по Московской области'),
+    menutitle   = REPLACE(menutitle,   'по Ленобласти', 'по Московской области');
+
+UPDATE modx_site_content SET
+    pagetitle   = REPLACE(pagetitle,   'Ленобласть', 'Московская область'),
+    longtitle   = REPLACE(longtitle,   'Ленобласть', 'Московская область'),
+    description = REPLACE(description, 'Ленобласть', 'Московская область'),
+    introtext   = REPLACE(introtext,   'Ленобласть', 'Московская область'),
+    content     = REPLACE(content,     'Ленобласть', 'Московская область'),
+    menutitle   = REPLACE(menutitle,   'Ленобласть', 'Московская область');
+
+UPDATE modx_site_content SET
+    pagetitle   = REPLACE(pagetitle,   ' ЛО ', ' МО '),
+    longtitle   = REPLACE(longtitle,   ' ЛО ', ' МО '),
+    description = REPLACE(description, ' ЛО ', ' МО '),
+    introtext   = REPLACE(introtext,   ' ЛО ', ' МО '),
+    content     = REPLACE(content,     ' ЛО ', ' МО ');
+
+UPDATE modx_site_content SET
+    pagetitle   = REPLACE(pagetitle,   ' СПб ', ' МСК '),
+    longtitle   = REPLACE(longtitle,   ' СПб ', ' МСК '),
+    description = REPLACE(description, ' СПб ', ' МСК '),
+    introtext   = REPLACE(introtext,   ' СПб ', ' МСК '),
+    content     = REPLACE(content,     ' СПб ', ' МСК '),
+    menutitle   = REPLACE(menutitle,   ' СПб ', ' МСК ');
+
+UPDATE modx_site_content SET
+    pagetitle   = REPLACE(pagetitle,   ' СПБ ', ' МСК '),
+    longtitle   = REPLACE(longtitle,   ' СПБ ', ' МСК '),
+    description = REPLACE(description, ' СПБ ', ' МСК '),
+    introtext   = REPLACE(introtext,   ' СПБ ', ' МСК '),
+    content     = REPLACE(content,     ' СПБ ', ' МСК '),
+    menutitle   = REPLACE(menutitle,   ' СПБ ', ' МСК ');
