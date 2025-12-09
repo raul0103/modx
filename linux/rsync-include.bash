e918457bcad359109408ce53c447fad55e047119
raul0103 <rshakurov95@mail.ru>
2025-12-09 14:06:20 +0500

    rsync

---
+++ linux/rsync-include.bash
@@ -0,0 +1,9 @@
+rsync -av \
+  --include='assets/' \
+  --include='assets/images/' \
+  --include='assets/images/products/' \
+  --include='assets/images/products/133304/***' \
+  --include='assets/images/products/133305/***' \
+  --exclude='*' \
+  /home/minvata/web/minvata.dev.skderdom.beget.tech/public_html/ \
+  root@193.42.125.65:/home/user/web/root.tsk-msk.gazobeton.waskuli.beget.tech/public_html/
