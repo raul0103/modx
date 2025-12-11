74067e14ced9a8e4f278cffdc0f6f0df5afe3f15
raul0103 <rshakurov95@mail.ru>
2025-12-11 15:57:22 +0500

    upd

--- linux/rsync-include.bash
+++ linux/rsync-include.bash
@@ -1,9 +1,16 @@
 rsync -av \
   --include='assets/' \
-  --include='assets/images/' \
-  --include='assets/images/products/' \
-  --include='assets/images/products/133304/***' \
-  --include='assets/images/products/133305/***' \
+  --include='/assets/template/' \
+  --include='/assets/template/img/' \
+  --include='/assets/template/img/import/' \
+  --include='/assets/template/img/import/c7910e43bedfbf6e6c6a99fc282d8171.pdf' \
+  --include='/assets/template/img/import/c80cd3b1460aa932a05117a99340965d.pdf' \
+  --include='/assets/template/img/import/6a811f6e5956c1d4e0eb54dde71fdbaa.pdf' \
+  --include='/assets/template/img/import/e09c5aab77db27f7146aec87bd263e9e.pdf' \
+  --include='/assets/template/img/import/54f6a0742cd33ab432b3c4f138997a53.pdf' \
+  --include='/assets/template/img/import/de94bc6a46482afa6d38a25bba1c675c.pdf' \
+  --include='/assets/template/img/import/d7663279d7069ef382ac8b6b015586fe.pdf' \
+  --include='/assets/template/img/import/5c6bbfee1ee89ce615a4754747536359.pdf' \
   --exclude='*' \
   /home/minvata/web/minvata.dev.skderdom.beget.tech/public_html/ \
   root@193.42.125.65:/home/user/web/root.tsk-msk.gazobeton.waskuli.beget.tech/public_html/
